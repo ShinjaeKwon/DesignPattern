b7358a9477052049f38f6777ad84fca8b18f9577
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-04-29 02:37:22 +0900

    feat : add comment

--- src/com/design/patterns/creational/abstractfatory/MainEntry.java
+++ src/com/design/patterns/creational/abstractfatory/MainEntry.java
@@ -8,6 +8,12 @@ import com.design.patterns.creational.abstractfatory.factory.LinuxFactory;
 
 public class MainEntry {
 
+	/**
+	 * Abstract Factory Pattern
+	 * 먼저 만들어야 할 컴포넌트 들을 추상적으로 정하고,
+	 * 어떤 구체적인 상황이 주어지면 각 컴포넌트들을 구체적으로 만드는 패턴
+	 */
+
 	public static void main(String[] args) {
 		ComponentFactory factory = new LinuxFactory();
 
@@ -18,7 +24,6 @@ public class MainEntry {
 		button.render();
 		checkBox.render();
 		textEdit.render();
-
 
 		button.clickEvent();
 		checkBox.setbChecked(true);
